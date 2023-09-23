c238ce839247a8f66d85701f41687672f1a0d791
limbongan <kbkmlimbongan@gmail.com>
2023-09-23 21:45:08 +0700

    membuat menu dashboard,logo dan nickname aplikasi

--- app/src/main/java/kbkm/th2023/limbonganready/MainActivity.java
+++ app/src/main/java/kbkm/th2023/limbonganready/MainActivity.java
@@ -2,7 +2,10 @@ package kbkm.th2023.limbonganready;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        VideoView video = (VideoView) findViewById(R.id.video1);
+        MediaController media = new MediaController(this);
+        Uri lokasivideo = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.opening);
+        video.setVideoURI(lokasivideo);
+        video.setMediaController(media);
+        video.start();
+        video.requestFocus();
     }
 }
