5e0f239e4b2ed41617fccba29f13e90f82ca4b16
naufal <hamaz.jozzz@gmail.com>
2023-11-02 13:35:42 +0700

    java setting pengaturan

--- app/src/main/java/kbkm/th2023/limbonganready/fragments/Profile.java
+++ app/src/main/java/kbkm/th2023/limbonganready/fragments/Profile.java
@@ -1,5 +1,6 @@
 package kbkm.th2023.limbonganready.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import kbkm.th2023.limbonganready.activities.Setting;
 import kbkm.th2023.limbonganready.R;
+
 
 public class Profile extends Fragment {
 
@@ -16,8 +20,17 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        ImageButton buttonSetting = view.findViewById(R.id.bt_setting);
 
+        buttonSetting.setOnClickListener(view1 -> {
+            // Kode untuk menangani klik tombol Setting di sini
+            Intent intent = new Intent(getActivity(), Setting.class);
+            startActivity(intent);
+        });
+
+        // Tambahkan pernyataan return di sini
+        return view;
     }
 }
